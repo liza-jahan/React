42949f145bfc582092abec41b66f8593ad5a3ab0
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-07-20 02:44:55 +0600

    Add files via upload

---
+++ EventHandelerOnChange/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EVENT_HANDLER_CLASS from  './EVENT_HANDLER_CLASS/index';
+function App() {
+  return (
+    <div className="App">
+   
+        <EVENT_HANDLER_CLASS />
+    </div>
+  );
+}
+
+export default App;
