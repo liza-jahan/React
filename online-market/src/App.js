117d4d0d1fd861d763d34c3692be5cd57ee926a8
Eakteker jahan Liza <74711092+liza-jahan@users.noreply.github.com>
2023-10-11 00:33:58 +0600

    Add files via upload

--- online-market/src/App.js
+++ online-market/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import TopNav from './componants/TopNav';
 import Featured from './componants/Featured';
 import Delivery from './componants/Delivery';
-
+import TopPicks from './componants/TopPicks';
 function App() {
   return (
     <div className="App">
       <TopNav/>
       <Featured/>
       <Delivery/>
+      <TopPicks/>
     </div>
   );
 }
---
+++ online-market/src/data/data.js
@@ -0,0 +1,131 @@
+export const topPicks = [
+  {
+    id: 1,
+    title: 'shawarma',
+    price: '$850',
+    img: 'https://www.lemonblossoms.com/wp-content/uploads/2020/06/Chicken-Shawarma-Recipe-S5.jpg'
+  },
+  {
+    id: 2,
+    title: 'fruite juice',
+    price: '$900',
+    img: 'https://content.jdmagicbox.com/comp/vizianagaram/q8/9999p8922.8922.171231223134.x1q8/catalogue/sricharanam-fruite-juice-and-milk-shake-vizianagaram-juice-centres-cyj6p.jpg?clr=',
+  },
+  {
+    id: 3,
+    title: 'yogurt parfaite',
+    price: '$450',
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSPzYSfezZ5gH3O-rM9hj9UwfiwZj5de9FTA&usqp=CAU"
+  },
+  {
+    id: 4,
+    title: 'orange juice',
+    price: '$420',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVrUe855-yaSHuZHRDPsUcemcxHdAskhsz8A&usqp=CAU'
+  },
+  {
+    id: 5,
+    title: 'egg & plantain',
+    price: '$700',
+    img: 'https://theonlinecook.com/wp-content/uploads/2023/03/fried-plantains-recipe.jpg',
+  },
+  {
+    id: 6,
+    title: 'tilapia fish',
+    price: '$900',
+    img: 'https://i.ytimg.com/vi/1LtXG6CQmZc/maxresdefault.jpg'
+  },
+  {
+    id: 7,
+    title: 'jollof rice',
+    price: '$150',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfnxVE1315jCR6DnaSkWCy3krEnz1_KiHWfJMLhWAEcHU2-AjtRZdgnDhy1PS-HidHjEI&usqp=CAU',
+  },
+  {
+    id: 8,
+    title: 'chiken burger',
+    price: '$300',
+    img: 'https://media.istockphoto.com/id/1273265655/photo/burger-with-fried-chicken-meat-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=iwDZMUYie0zldQBJY6vXjidTqw6rNKrSEnSPszXpJCI=',
+  }
+
+];
+
+
+export const mealData = [
+  {
+    id: 5,
+    name: 'cheeseless pizza',
+    category: 'pizza',
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJEbUK3VNxsHasF-oM-dB1m3f8qss5opa1vw&usqp=CAU',
+    price: '$200',
+  },
+  {
+    id: 6,
+    name: 'Supreme Pizza',
+    category: 'pizza',
+    image:
+      'https://media.istockphoto.com/id/1151447052/photo/tasty-supreme-pizza-with-olives-peppers-onions-and-sausage.jpg?s=612x612&w=0&k=20&c=6m17pHx7VmeR2jCYmgbVCMTUgejpzFsB4Sx654fPBnM=',
+    price: '$300',
+  },
+  {
+    id: 7,
+    name: 'pepperoni pizza',
+    category: 'pizza',
+    image:
+      'https://www.simplyrecipes.com/thmb/KE6iMblr3R2Db6oE8HdyVsFSj2A=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-1024x682-583b275444104ef189d693a64df625da.jpg',
+    price: '$100',
+  },
+  {
+    id: 8,
+    name: 'Cheese Pizza',
+    category: 'pizza',
+    image:
+      'https://img.freepik.com/premium-photo/aesthetic-dripping-tasty-pizza-slice-generative-ai_863013-1954.jpg',
+    price: '$140',
+  }
+
+];
+
+
+
+  export const categories = [
+    {
+      id: 1,
+      name: 'Burger',
+      image:
+         'https://images.unsplash.com/photo-1586190848861-99aa4a171e90?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnVyZ2VyfGVufDB8fDB8fHww&w=1000&q=80'
+    },
+    {
+      id: 2,
+      name: 'veg',
+      image:
+        'https://media.istockphoto.com/id/1203599923/photo/food-background-with-assortment-of-fresh-organic-vegetables.webp?b=1&s=170667a&w=0&k=20&c=fRNCED4dyey-i6K2RHTPaIm_HFLUr3hnj4J6WblHaXc='
+    },
+    {
+      id: 3,
+      name: 'Pizza',
+      image:
+        'https://img.freepik.com/premium-photo/aesthetic-dripping-tasty-pizza-slice-generative-ai_863013-1954.jpg',
+    },
+    {
+      id: 4,
+      name: 'Wings',
+      image:
+        'https://media.istockphoto.com/id/532607409/photo/chicken-wings.jpg?s=612x612&w=0&k=20&c=JKyzVvVvPIkGoAaN-dBHyrsyAVtB9Q1Mimv2SPv88Yk='
+    },
+
+    {
+      id: 5,
+      name: 'Best Overall',
+      image:
+        'https://res.cloudinary.com/ehizeex-shop/image/upload/v1672070207/NetflixApp/fastBest Overallfood_logo_v80nbk.png'
+    },
+    {
+      id: 6,
+      name: 'sharwama',
+      image:
+        'https://img.freepik.com/free-photo/fresh-gourmet-sandwich-with-meat-vegetables-generative-ai_188544-8106.jpg?size=626&ext=jpg&ga=GA1.1.1413502914.1696896000&semt=sph'
+    }
+    
+  ];
